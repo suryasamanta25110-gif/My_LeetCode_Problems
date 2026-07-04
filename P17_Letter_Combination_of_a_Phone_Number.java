1d9e833efecca07c074787baf5fb0e866856ea8e
suryasamanta25110-gif <suryasamanta25110@gmail.com>
2026-07-04 12:22:23 +0530

    Program 17
    I am thrilled to make this repository and share my LeetCode solutions to others.
    
    Starting with one of my exciting problem, when I was learning backtracking. I challenged my learning with a problem on the topic, and to my surprise, it ran on the first try. This is the question that I attempted at that time.
    
    I will keep sharing whatever problems I solve in LeetCode.

---
+++ P17_Letter_Combination_of_a_Phone_Number.java
@@ -0,0 +1,44 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class P17_Letter_Combination_of_a_Phone_Number {
+
+    // LeetCode main function.
+    private List<String> letterCombinations(String digits) {
+
+        // Initializing variables and objects.
+        List<String> combo = new ArrayList<>();
+        String[] data = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "weyx"};
+
+        // Calling function to find combinations.
+        findCombinations(0, digits, data, new StringBuilder(), combo);
+
+        // Returning the list of combinations.
+        return combo;
+    }
+
+    // Recursive function to find the letter combinations.
+    private void findCombinations(int index, String digits, String[] data, StringBuilder temp, List<String> combo) {
+
+        // Base case.
+        if (index == digits.length()) {
+            combo.add(temp.toString());
+            return;
+        }
+
+        // Finding the combinations.
+        for (int i = 0; i < data[digits.charAt(index) - '2'].length(); i++) {
+            temp.append(data[digits.charAt(index) - '2'].charAt(i));
+            findCombinations(index + 1, digits, data, temp, combo);
+            temp.deleteCharAt(temp.length() - 1);
+        }
+    }
+
+    // Main method to call LeetCode main function and run the code.
+    public static void main(String[] args) {
+        P17_Letter_Combination_of_a_Phone_Number obj = new P17_Letter_Combination_of_a_Phone_Number();
+        Scanner sc = new Scanner(System.in);
+        System.out.println(obj.letterCombinations(sc.next()));
+    }
+}
